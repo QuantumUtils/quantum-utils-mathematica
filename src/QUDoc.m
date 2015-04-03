7e08d879b6ae00d0c382d1f91bf2ddb463975bee
Ian Hincks <ian.hincks@gmail.com>
2015-04-03 12:26:42 -0400

    Forgot to remove Print statement.

--- src/QUDoc.m
+++ src/QUDoc.m
@@ -3,13 +3,12 @@
 BeginPackage["QUDoc`"];
 Needs["QUDevTools`"];
 
-(* Only open index.nb if Get (<<) was used to run this file. *)
-(* Stack will be {Needs, Get, ...} if Needs is used (and remember *)
-(* the package won't be reloaded by Needs if it has already been *) 
-(* loaded). Stack will be {BeginPackage, Needs, Get, ...} if *)
-(* BeginPackage[..,{..,"QUDoc`",...}] is used. *)
+(* Only open index.nb if Get (<<) was used to run this file.       *)
+(* Stack will be {Needs, Get, ...} if Needs is used (and remember  *)
+(* the package won't be reloaded by Needs if it has already been   *) 
+(* loaded). Stack will be {BeginPackage, Needs, Get, ...} if        *)
+(* BeginPackage[..,{..,"QUDoc`",...}] is used.                       *)
 $stack=Stack[];
-Print[$stack];
 If[First@$stack===Get,
 	NotebookOpen[FileNameJoin[{$QUDocumentationPath, "index.nb"}]];
 ];
