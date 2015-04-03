deed65f86fbcd3dd7f19028c42ad327d3a15f5c6
Ian Hincks <ian.hincks@gmail.com>
2015-04-03 12:23:45 -0400

    Figured out how to make index.nb open only if Get was used.

--- src/QUDoc.m
+++ src/QUDoc.m
@@ -2,5 +2,16 @@
 
 BeginPackage["QUDoc`"];
 Needs["QUDevTools`"];
-NotebookOpen[FileNameJoin[{$QUDocumentationPath, "index.nb"}]];
+
+(* Only open index.nb if Get (<<) was used to run this file. *)
+(* Stack will be {Needs, Get, ...} if Needs is used (and remember *)
+(* the package won't be reloaded by Needs if it has already been *) 
+(* loaded). Stack will be {BeginPackage, Needs, Get, ...} if *)
+(* BeginPackage[..,{..,"QUDoc`",...}] is used. *)
+$stack=Stack[];
+Print[$stack];
+If[First@$stack===Get,
+	NotebookOpen[FileNameJoin[{$QUDocumentationPath, "index.nb"}]];
+];
+
 EndPackage[];
